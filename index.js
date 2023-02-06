2b254fd48ae950930f71621cc134808a5157c321
Ronak Lala <ronaklala2010@gmail.com>
2023-02-06 15:27:14 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+var cors = require("cors");
+const { default: mongoose } = require("mongoose");
+var bodyParser = require("body-parser");
+
+const app = express();
+app.use(cors());
+
+const DB =
+  "mongodb+srv://Warrior:Ronak3103@mydb.tgsvt.mongodb.net/Insurance?retryWrites=true&w=majority";
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connected");
+  })
+  .catch((err) => console.log(err));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
+app.use(bodyParser.json());
+
+app.listen(process.env.PORT || 5000);
